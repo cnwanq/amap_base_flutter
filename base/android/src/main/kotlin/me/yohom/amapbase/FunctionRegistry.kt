6913bf79985405be5f14a3437332c7e6cfec1d33
Wan Qijian <wanqijian@fansit.net>
2019-10-13 18:02:10 +0800

    update codes

--- base/android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
+++ base/android/src/main/kotlin/me/yohom/amapbase/FunctionRegistry.kt
@@ -10,31 +10,31 @@ import me.yohom.amapbase.search.*
 /**
  * 地图功能集合
  */
-val MAP_METHOD_HANDLER: Map<String, MapMethodHandler> = mapOf(
-        "map#setMyLocationStyle" to SetMyLocationStyle,
-        "map#setUiSettings" to SetUiSettings,
-        "marker#addMarker" to AddMarker,
-        "marker#addMarkers" to AddMarkers,
-        "marker#clear" to ClearMarker,
-        "map#showIndoorMap" to ShowIndoorMap,
-        "map#setMapType" to SetMapType,
-        "map#setLanguage" to SetLanguage,
-        "map#clear" to ClearMap,
-        "map#setZoomLevel" to SetZoomLevel,
-        "map#setPosition" to SetPosition,
-        "map#setMapStatusLimits" to SetMapStatusLimits,
-        "tool#convertCoordinate" to ConvertCoordinate,
-        "tool#calcDistance" to CalcDistance,
-        "offline#openOfflineManager" to OpenOfflineManager,
-        "map#addPolyline" to AddPolyline,
-        "map#zoomToSpan" to ZoomToSpan,
-        "map#screenshot" to ScreenShot,
-        "map#setCustomMapStylePath" to SetCustomMapStylePath,
-        "map#setMapCustomEnable" to SetMapCustomEnable,
-        "map#setCustomMapStyleID" to SetCustomMapStyleID,
-        "map#getCenterPoint" to GetCenterLnglat,
-        "map#changeLatLng" to ChangeLatLng
-)
+//val MAP_METHOD_HANDLER: Map<String, MapMethodHandler> = mapOf(
+//        "map#setMyLocationStyle" to SetMyLocationStyle,
+//        "map#setUiSettings" to SetUiSettings,
+//        "marker#addMarker" to AddMarker,
+//        "marker#addMarkers" to AddMarkers,
+//        "marker#clear" to ClearMarker,
+//        "map#showIndoorMap" to ShowIndoorMap,
+//        "map#setMapType" to SetMapType,
+//        "map#setLanguage" to SetLanguage,
+//        "map#clear" to ClearMap,
+//        "map#setZoomLevel" to SetZoomLevel,
+//        "map#setPosition" to SetPosition,
+//        "map#setMapStatusLimits" to SetMapStatusLimits,
+//        "tool#convertCoordinate" to ConvertCoordinate,
+//        "tool#calcDistance" to CalcDistance,
+//        "offline#openOfflineManager" to OpenOfflineManager,
+//        "map#addPolyline" to AddPolyline,
+//        "map#zoomToSpan" to ZoomToSpan,
+//        "map#screenshot" to ScreenShot,
+//        "map#setCustomMapStylePath" to SetCustomMapStylePath,
+//        "map#setMapCustomEnable" to SetMapCustomEnable,
+//        "map#setCustomMapStyleID" to SetCustomMapStyleID,
+//        "map#getCenterPoint" to GetCenterLnglat,
+//        "map#changeLatLng" to ChangeLatLng
+//)
 
 /**
  * 搜索功能集合
@@ -56,9 +56,9 @@ val SEARCH_METHOD_HANDLER: Map<String, SearchMethodHandler> = mapOf(
 /**
  * 导航功能集合
  */
-val NAVI_METHOD_HANDLER: Map<String, NaviMethodHandler> = mapOf(
-        "navi#startNavi" to StartNavi
-)
+//val NAVI_METHOD_HANDLER: Map<String, NaviMethodHandler> = mapOf(
+//        "navi#startNavi" to StartNavi
+//)
 
 /**
  * 定位功能集合
